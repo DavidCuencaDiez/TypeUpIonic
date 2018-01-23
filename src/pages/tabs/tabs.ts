693e9cb887badcd53b97cf41739ca1c15bd44b7d
David <xivi30@gmail.com>
2018-01-23 17:19:08 +0100

    - Ionic project

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+import { SearchPage } from '../search/search';
+import { AboutPage } from '../about/about';
+import { ContactPage } from '../contact/contact';
+import { HomePage } from '../home/home';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = HomePage;
+  tab2Root = SearchPage;
+  tab3Root = AboutPage;
+  tab4Root = ContactPage;
+  constructor() {
+
+  }
+}
